2245c5374afcf6341d1a752ab8c86f4cba0c8b8d
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-08-01 13:59:44 +0200

    anonymous classes example

---
+++ src/advanced/anonymous/ForestDemo.java
@@ -0,0 +1,33 @@
+package advanced.anonymous;
+//Stwórz obiekt Tree z metodą display() metoda ta ma drukować “wyświetlam zwykłe drzewo”.
+// Stwórz listę 100 drzew i nazwij ją forest. Do zbioru, za pomocą anonimowej klasy, dodaj jedno specjalne drzewo,
+// które będzie wyświetlać “trafiłeś na magiczne drzewo!”. Dodaj je do zbioru a zbiór wmieszaj metodą
+// Collections.shuffle(List). Przeiteruj się po “lesie”
+// i wyświetl wszystkie drzewa za pomocą metody display() :) Jedno z nich powinno wykazać szczególne zachowanie.
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ForestDemo {
+    public static void main(String[] args) {
+        List<Tree> forest = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            forest.add(new Tree());
+        }
+
+        Tree specialTree = new Tree(){
+            @Override
+            public void display() {
+                System.out.println("trafiłeś na magiczne drzewo!");
+            }
+        };
+
+        forest.add(specialTree);
+
+        Collections.shuffle(forest);
+        for (Tree tree : forest) {
+            tree.display();
+        }
+    }
+}
---
+++ src/advanced/anonymous/Tree.java
@@ -0,0 +1,7 @@
+package advanced.anonymous;
+
+public class Tree {
+    public void display() {
+        System.out.println("Wyświetlam zwykłe drzewo");
+    }
+}
