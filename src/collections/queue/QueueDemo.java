8523375294a756106109bbbc50fb7d141dddad26
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-08-01 12:47:39 +0200

    queue examples

---
+++ src/collections/queue/QueueDemo.java
@@ -0,0 +1,46 @@
+package collections.queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+        //FIFO
+//        Queue<String> names =new LinkedBlockingQueue<>(3);
+        Queue<String> names =new ArrayBlockingQueue<>(3);
+        names.add("Wojtek");
+        names.add("Daniel");
+        names.add("Ania");
+        names.add("Gosia");
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.element());
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.element());
+
+        //add, element remove -> rzucają wyjątki
+        names.offer("Wojtek");
+        names.offer("Daniel");
+        names.offer("Ania");
+        names.offer("Gosia");
+
+        System.out.println(names);
+
+        System.out.println(names.poll());
+        System.out.println(names.poll());
+        System.out.println(names.poll());
+        System.out.println(names.poll());
+        System.out.println(names.peek());
+
+
+
+    }
+
+
+}
